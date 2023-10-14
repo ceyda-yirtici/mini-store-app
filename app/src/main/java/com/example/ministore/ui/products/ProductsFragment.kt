fca27f50005b33b026ddec14c604bd5499221224
Ceyda Yırtıcı <ceydayrtc@gmail.com>
2023-10-14 13:35:21 +0300

    product adding/removing view & toolbar view

--- app/src/main/java/com/example/ministore/ui/products/ProductsFragment.kt
+++ app/src/main/java/com/example/ministore/ui/products/ProductsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
@@ -32,7 +33,6 @@ class ProductsFragment: Fragment(R.layout.fragment_products)  {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initView(view)
         listenViewModel()
     }
